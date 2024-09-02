6609dcd67c8d87f5577f801c5357098309713785
MJ Akbari <mojav.akbari@gmail.com>
2024-09-02 14:44:07 +0330

    debug

--- discussion_board/settings.py
+++ discussion_board/settings.py
@@ -44,6 +44,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FormParser',
+    ),
 }
 
 SIMPLE_JWT = {
