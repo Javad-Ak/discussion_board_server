2bfc99098a5b14de1ca61f0c902243577d267f8a
MJ Akbari <mojav.akbari@gmail.com>
2024-09-03 17:40:43 +0330

    refactor

--- discussions/serializers.py
+++ discussions/serializers.py
@@ -11,15 +11,16 @@ class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         exclude = ['owner']
-        extra_kwargs = {'pk': {'read_only': True},}
+        extra_kwargs = {'pk': {'read_only': True}, }
 
 
 class CommentSerializer(serializers.ModelSerializer):
     """Serializer for the Comment model"""
     username = serializers.ReadOnlyField(source='owner.username')
     avatar = serializers.ImageField(source='owner.avatar', read_only=True)
+    topic_pk = serializers.ReadOnlyField(source='topic.pk')
 
     class Meta:
         model = Comment
         exclude = ['owner', 'topic']
-        extra_kwargs = {'pk': {'read_only': True},}
+        extra_kwargs = {'pk': {'read_only': True}, }
